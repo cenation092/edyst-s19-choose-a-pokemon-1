b8fc2e337942f48f1f5d70c03ecf743eda562158
Ganesh Chandra <35104706+Ganesh-Chandra@users.noreply.github.com>
2019-04-21 00:27:39 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,22 @@
-
+#imports the required modules Flask,json
 from flask import Flask
 import json
 
 app = Flask(__name__)
 
+#handels the wrong url constructions.
 @app.errorhandler(404)
 def unknown(a):
     return "Please check your url and try again."
 
+#main route for the api call i.e api/pokemon 
 @app.route('/api/pokemon')
 def home():
+    
     data=["bulbasaur","charmander", "squirtle"]
-    data={"pokemon":data}
-    return json.dumps(data)
+    data={"pokemon":data}#dict that has the pokemon data
+    return json.dumps(data)#dict converted to json format using dumps method.
 
 if __name__ == '__main__':
-    app.run(port=8006,debug=True)
+    app.run(port=8006,debug=True)#check out the port in which app runs.
 
