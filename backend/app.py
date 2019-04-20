da70e8ed178ea175889b16da311c20f0a35968a1
Ganesh-Chandra <ganeshchandrasaladi@gmail.com>
2019-04-20 20:23:46 +0530

    executed.documenting left

--- backend/app.py
+++ backend/app.py
@@ -1,25 +1,19 @@
-from flask import *
-import os
+
+from flask import Flask
+import json
 
 app = Flask(__name__)
-@app.context_processor
-def override_url_for():
-    
-    return dict(url_for=dated_url_for)
 
-def dated_url_for(endpoint, **values):
-    if endpoint == 'static':
-        filename = values.get('filename', None)
-        print(values,filename,app.root_path)
-        if filename:
-            file_path = os.path.join(app.root_path,
-                                 endpoint, filename)
-            values['q'] = int(os.stat(file_path).st_mtime)
-    return url_for(endpoint, **values)
+@app.errorhandler(404)
+def unknown(a):
+    return "Please check your url and try again."
 
 @app.route('/api/pokemon')
 def home():
-    return render_template("gun.html")
+    data=["bulbasaur","charmander", "squirtle"]
+    data={"pokemon":data}
+    return json.dumps(data)
+
 if __name__ == '__main__':
     app.run(port=8006,debug=True)
 
