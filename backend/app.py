d0992b9e0542a551c092ab367ab4526c6d9fb693
Ganesh Chandra <35104706+Ganesh-Chandra@users.noreply.github.com>
2019-04-21 01:47:02 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 #handels the wrong url constructions.
 @app.errorhandler(404)
-def unknown(a):
-    return "Please check your url and try again."
+def page_not_found(e):
+	return render_template("404.html")
 
 #main route for the api call i.e api/pokemon 
 @app.route('/api/pokemon')
